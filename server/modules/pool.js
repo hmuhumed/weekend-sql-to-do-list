f348e040b00e14bf3f2df13c64ed5592c242cfb8
Hamza Muhumed <hamzamuhumed2107@gmail.com>
2023-05-19 16:51:33 -0500

    Installed my node modules and package.json

---
+++ server/modules/pool.js
@@ -0,0 +1,16 @@
+const pg = require('pg');
+const pool = new pg.Pool({
+
+    // name of our database
+    // this will change for every app
+    database: 'weekend-to-do-app',
+
+    // where our db is
+    // localhost === on your computer
+    host: 'localhost',
+
+    // Postgres listens for network connection on port 5432 by default
+    port: 5432
+})
+
+module.exports = pool
