76fb8c3ab56dc1701884edda11123b0e010eee1f
Hamza Muhumed <hamzamuhumed2107@gmail.com>
2023-05-19 18:20:00 -0500

    Created my database and made a table with values inserted

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "todo" (
+"todo" VARCHAR(50) NOT NULL,
+"completed" VARCHAR(4) DEFAULT 'no'
+);
+
+
+
+INSERT INTO "todo" ("todo") VALUES ('Go to the gym!');
---
+++ server/router/task.router.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const taskRouter = express.Router();
+const pool = require('../modules/pool/');
+
+// GET
+
+
+
+
+// POST
+
+
+
+
+// PUT
+
+
+
+
+
+// DELETE
